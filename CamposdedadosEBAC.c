974430c2a1d66d84af43bc2383cdb5b5d174598d
Vitória Barcellos <vbarcellost@gmail.com>
2024-09-10 01:53:29 -0300

    Add files via upload
    Opção 4 adicionada, opção: Sair do sistema.

--- CamposdedadosEBAC.c
+++ CamposdedadosEBAC.c
@@ -129,9 +129,10 @@ int main ()
 	
 		printf("### Campo de dados EBAC ###\n\n"); //inicio do menu
 		printf("Escolha a opção desejada no menu:\n\n");
-		printf("\t1- Registrar nomes\n");
-		printf("\t2- Consultar nomes\n");
-			printf("\t3- Deletar nomes\n"); 
+		printf("\t1 - Registrar nomes\n");
+		printf("\t2 - Consultar nomes\n");
+		printf("\t3 - Deletar nomes\n"); 
+		printf("\t4 - Sair do Sistema\n");
 		printf("Opção: "); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -151,6 +152,11 @@ int main ()
 			
 			case 3: 
 			deletar();
+			break;
+		
+			case 4: 
+			printf("Obrigado por utilizar o sistema!");
+			return 0;
 			break;
 			
 			default: 	
